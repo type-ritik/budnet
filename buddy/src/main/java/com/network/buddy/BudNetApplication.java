6f22c10578d2a04381445659c3d04371f281a078
rksvw <ritik.vw98@gmail.com>
2025-12-10 17:29:48 +0530

    backend setup

---
+++ buddy/src/main/java/com/network/buddy/BudNetApplication.java
@@ -0,0 +1,13 @@
+package com.network.buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudNetApplication.class, args);
+	}
+
+}
---
+++ buddy/src/test/java/com/network/buddy/BudNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.network.buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
