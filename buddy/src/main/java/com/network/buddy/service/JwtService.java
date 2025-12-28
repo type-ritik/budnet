94b3a61f2eb8125a13fa5670440c1f2ee40af01c
rksvw <ritik.vw98@gmail.com>
2025-12-28 10:41:23 +0530

    implement JwtService

---
+++ buddy/src/main/java/com/network/buddy/service/JwtService.java
@@ -0,0 +1,71 @@
+package com.network.buddy.service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import com.network.buddy.model.UserEntity;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+    // Environment data import
+    @Value("$application.security.jwt.secret-key}")
+    private String SEC_KEY;
+    @Value("$application.security.jwt.expiration}")
+    private long expiration;
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SEC_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public Date extractExpiration(String token) {
+        return extractAllClaims(token).getExpiration();
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder().build().parseClaimsJws(token).getBody();
+    }
+
+    private String createToken(Map<String, Object> claims, String username) {
+        return Jwts.builder().setClaims(claims).setSubject(username).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    public String generateToken(UserEntity user) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", user.getId());
+        claims.put("username", user.getUsername());
+        claims.put("email", user.getEmail());
+        claims.put("role", user.getRole());
+        return createToken(claims, user.getUsername());
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        Date expireDate = extractExpiration(token);
+        if (expireDate.before(new Date())) {
+            return false;
+        }
+
+        String username = extractUsername(token);
+        return userDetails.getUsername().equals(username) && !expireDate.before(new Date());
+    }
+
+}
