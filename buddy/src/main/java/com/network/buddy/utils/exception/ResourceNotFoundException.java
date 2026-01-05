0b242ca0b8dac8a8f196d20b6bda5d4b6e649987
rksvw <ritik.vw98@gmail.com>
2026-01-05 15:22:00 +0530

    Custom exception response both success and errors

---
+++ buddy/src/main/java/com/network/buddy/utils/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.network.buddy.utils.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
