3d4bfdfc5867368ffe7a8a4ba2cbcb9b25a94d43
rksvw <ritik.vw98@gmail.com>
2026-01-05 15:38:28 +0530

    Refactor exception handling in services and controllers to use custom exceptions for better error management

--- buddy/src/main/java/com/network/buddy/controller/PostController.java
+++ buddy/src/main/java/com/network/buddy/controller/PostController.java
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.network.buddy.dto.CreatePost.CreatePostRequest;
+import com.network.buddy.dto.CreatePost.CreatePostResponse;
+import com.network.buddy.model.ApiResponse;
 import com.network.buddy.service.PostService;
-
+import com.network.buddy.utils.ResponseUtil;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,15 +25,12 @@ public class PostController {
     }
 
     @PostMapping("/create-post")
-    public ResponseEntity<?> createPost(@Valid @RequestBody CreatePostRequest request) {
-        try {
-            log.info("Create Post component hit");
-            log.info("Request info: " + request.toString());
-            var response = postService.makePost(request);
-            log.info("Create Post component end");
-            return ResponseEntity.ok(response);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body("Error: " + e.getLocalizedMessage());
-        }
+    public ResponseEntity<ApiResponse<CreatePostResponse>> createPost(@Valid @RequestBody CreatePostRequest request) {
+
+        log.info("Create Post component hit");
+        log.info("Request info: " + request.toString());
+        var response = postService.makePost(request);
+        log.info("Create Post component end");
+        return ResponseEntity.ok(ResponseUtil.success(response, "Post created successfully!", "/"));
     }
 }
