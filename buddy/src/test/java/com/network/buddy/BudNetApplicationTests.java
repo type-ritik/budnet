a95277f7fa85d3eae7a0737cca829f9c87a8fff0
rksvw <ritik.vw98@gmail.com>
2026-01-14 20:15:06 +0530

    test: implement 3 example test and 1 hello api test

--- buddy/src/test/java/com/network/buddy/BudNetApplicationTests.java
+++ buddy/src/test/java/com/network/buddy/BudNetApplicationTests.java
@@ -1,5 +1,7 @@
 package com.network.buddy;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,16 @@ class BudNetApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void addTest() {
+		assertEquals(5, 2 + 3);
+	}
+
+	@Test
+	void subTest() {
+		assertEquals(6, 10 - 4);
 	}
 
 }
